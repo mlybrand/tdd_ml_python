0ad6d61d78b698355339b5f49056a0e87b8be1a3
Mark Lybrand <mlybrand@gmail.com>
2015-12-02 10:17:12 -0800

    implemented second test example

--- ch01/NumberGuesser.py
+++ ch01/NumberGuesser.py
@@ -1,4 +1,8 @@
 class NumberGuesser:
     """Guesses numbers based on the history of your input"""
+    def __init__(self):
+        self._guessed_number = None
+    def number_was(self, guessed_number):
+        self._guessed_number = guessed_number
     def guess(self):
-        return None
+        return self._guessed_number
--- ch01/number_guesser_tests.py
+++ ch01/number_guesser_tests.py
@@ -4,3 +4,11 @@ def given_no_information_when_asked_to_guess_test():
     number_guesser = NumberGuesser()
     result = number_guesser.guess()
     assert result is None, "Then it should provide no result."
+
+def given_one_datapoint_when_asked_to_guess_test():
+    number_guesser = NumberGuesser()
+    previously_chosen_number = 5
+    number_guesser.number_was(previously_chosen_number)
+    guessed_number = number_guesser.guess()
+    assert type(guessed_number) is int, 'the answer should be a number'
+    assert guessed_number == previously_chosen_number, 'the answer should be previously chosen number.'
