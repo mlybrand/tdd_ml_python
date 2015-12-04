1c6f857d4c0bdee5561a191ed496c6d875f36989
Mark Lybrand <mlybrand@gmail.com>
2015-12-03 18:43:23 -0800

    another test passing

--- ch01/NumberGuesser.py
+++ ch01/NumberGuesser.py
@@ -2,6 +2,8 @@ class NumberGuesser:
     """Guesses numbers based on the history of your input"""
     def __init__(self):
         self._guessed_number = None
+    def numbers_were(self, guessed_numbers):
+        self._guessed_number = guessed_numbers[0]
     def number_was(self, guessed_number):
         self._guessed_number = guessed_number
     def guess(self):
--- ch01/number_guesser_tests.py
+++ ch01/number_guesser_tests.py
@@ -12,3 +12,10 @@ def given_one_datapoint_when_asked_to_guess_test():
     guessed_number = number_guesser.guess()
     assert type(guessed_number) is int, 'the answer should be a number'
     assert guessed_number == previously_chosen_number, 'the answer should be previously chosen number.'
+
+def given_two_datapoints_when_asked_to_guess_test():
+    number_guesser = NumberGuesser()
+    previously_chosen_numbers = [1,2,5]
+    number_guesser.numbers_were(previously_chosen_numbers)
+    guessed_number = number_guesser.guess()
+    assert guessed_number in previously_chosen_numbers, 'the guess should be one the previously chosen numbers'
