2a72c43393337e773981cc4d39eb828b62c25b98
Mark Lybrand <mlybrand@gmail.com>
2016-02-04 12:38:46 -0800

    added another test

--- ch01/number_guesser_tests.py
+++ ch01/number_guesser_tests.py
@@ -19,3 +19,25 @@ def given_two_datapoints_when_asked_to_guess_test():
     number_guesser.numbers_were(previously_chosen_numbers)
     guessed_number = number_guesser.guess()
     assert guessed_number in previously_chosen_numbers, 'the guess should be one the previously chosen numbers'
+
+def given_multiple_datapoints_when_asked_to_guess_many_times_test():
+    number_guesser = NumberGuesser()
+    previously_chosen_numbers = [1,2,5]
+    number_guesser.numbers_were(previously_chosen_numbers)
+    guessed_numbers = [number_guesser.guess() for i in range(0,100)]
+    for guessed_number in guessed_numbers:
+        assert guessed_number in previously_chosen_numbers, 'every guess should be one of the previously chosen numbers'
+        assert len(set(guessed_numbers)) > 1, "It shouldn't always guess the same number"
+
+def given_a_starting_set_of_observations_followed_by_a_one_off_observation_test():
+    number_guesser = NumberGuesser()
+    previously_chosen_numbers = [1, 2, 5]
+    number_guesser.numbers_were(previously_chosen_numbers)
+    one_off_observation = 0
+    number_guesser.number_was(one_off_observation)
+
+    guessed_numbers = [number_guesser.guess() for i in range(0, 100)]
+
+    for guessed_number in guessed_numbers:
+        assert guessed_number in previously_chosen_numbers + [one_off_observation], 'every guess should be one of the previously chosen numbers'
+        assert len(set(guessed_numbers)) > 1, "It shouldn't always guess the same number"
